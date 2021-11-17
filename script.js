1565b9b67b666eb4af77ce127f9c058a834a6670
J <j@Js-MacBook-Air.local>
2021-11-17 15:33:41 +0100

    Carrousel

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function () {
     // Variables
 
     // Añadir las tres imágenes del directorio "img" al array IMAGENES.
-    const IMAGENES = [];
+    const IMAGENES = ['img/img1.jpg', 'img/img2.jpg', 'img/img3.jpg'];
 
     const TIEMPO_INTERVALO_MILESIMAS_SEG = 1000;
 
@@ -10,11 +10,11 @@ window.onload = function () {
     let posicionActual = 0;
 
     // variables con los elementos del DOM HTML, aplicar el selector necesario.
-    let $botonRetroceder
-    let $botonAvanzar 
-    let $imagen 
-    let $botonPlay 
-    let $botonStop
+    let $botonRetroceder = document.getElementById("retroceder");
+    let $botonAvanzar = document.getElementById("avanzar");
+    let $imagen = document.getElementById("imagen");
+    let $botonPlay = document.getElementById("play");
+    let $botonStop = document.getElementById("stop");
 
     // Identificador del proceso que se ejecuta con setInterval().
     let intervalo;
@@ -25,8 +25,12 @@ window.onload = function () {
      * Funcion que cambia la foto en la siguiente posicion
      */
     function pasarFoto() {
+        posicionActual++;
+        if (posicionActual >= IMAGENES.length) {
+            posicionActual = 0;
+        }
+        renderizarImagen();
         // se incrementa el indice (posicionActual)
-
         // ...y se muestra la imagen que toca.
     }
 
@@ -34,6 +38,11 @@ window.onload = function () {
      * Funcion que cambia la foto en la anterior posicion
      */
     function retrocederFoto() {
+        posicionActual--;
+        if (posicionActual < 0) {
+            posicionActual = IMAGENES.length - 1;
+        }
+        renderizarImagen();
         // se incrementa el indice (posicionActual)
 
         // ...y se muestra la imagen que toca.
@@ -52,10 +61,12 @@ window.onload = function () {
     function playIntervalo() {
         // Documentación de la función setInterval: https://developer.mozilla.org/en-US/docs/Web/API/setInterval
         // Mediante la función setInterval() se ejecuta la función pasarFoto cada TIEMPO_INTERVALO_MILESIMAS_SEG.
-        
-
+        intervalo = setInterval(pasarFoto, TIEMPO_INTERVALO_MILESIMAS_SEG);
         // Desactivamos los botones de control necesarios. Utilizando setAttribute y removeAttribute.
-
+        $botonPlay.setAttribute("disabled", "");
+        $botonRetroceder.setAttribute("disabled", "");
+        $botonAvanzar.setAttribute("disabled", "");
+        $botonStop.removeAttribute("disabled", "");
     }
 
     /**
@@ -63,13 +74,59 @@ window.onload = function () {
      */
     function stopIntervalo() {
         // Desactivar la ejecución de intervalo.
-
+        clearInterval(intervalo);
+        intervalo = null;
         // Activamos los botones de control. Utilizando setAttribute y removeAttribute.
+        $botonStop.setAttribute("disabled", "");
+        $botonRetroceder.removeAttribute("disabled", "");
+        $botonAvanzar.removeAttribute("disabled", "");
+        $botonPlay.removeAttribute("disabled", "");
     }
 
     // Eventos
     // Añadimos los evenntos necesarios para cada boton. Mediante addEventListener.
-
+    $botonAvanzar.addEventListener("click", pasarFoto);
+    $botonRetroceder.addEventListener("click", retrocederFoto);
+    $botonPlay.addEventListener("click", playIntervalo);
+    $botonStop.addEventListener("click", stopIntervalo);
     // Iniciar
     renderizarImagen();
-} 
+
+    document.onkeydown = checkKey;
+
+    function checkKey(e) {
+        e = e || window.event;
+        if (e.keyCode == '37') {
+            // left arrow
+            pasarFoto();
+        } else if (e.keyCode == '39') {
+            // right arrow
+            retrocederFoto();
+        }
+
+    }
+
+    $imagen.innerText = `url(${IMAGENES[posicionActual]})`;
+
+    function leaveInfo() {
+        $image.innerText = "";
+    }
+
+    let circle = document.getElementById('circle');
+    const onMouseMove = (e) => {
+        circle.style.left = e.pageX + 'px';
+        circle.style.top = e.pageY + 'px';
+    }
+    document.addEventListener('mousemove', onMouseMove)
+
+    function getName() {
+        var fullPath = document.getElementById("img1").src;
+        var filename = fullPath.replace(/^.*[\\\/]/, '');
+        // or, try this,
+        // var filename = fullPath.split("/").pop();
+        document.getElementById("result").value = filename;
+    }
+
+}
+
+
